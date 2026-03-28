ee7bc50b5fe5320ae66e8de9c2c86d2f651ab2eb
David A. G. Harrison <harrison@cs.olemiss.edu>
2026-03-27 20:12:49 -0500

    Tell playwright to run one test at a time so as to avoid race conditions.

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './playwright-tests',
   timeout: 30_000,
+  workers: 1,
   use: {
     baseURL: 'http://127.0.0.1:8888'
   },
